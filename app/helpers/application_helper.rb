88db96b858d7aa68114f3d118ffec6795d2a3dbd
haazime <h4zime@gmail.com>
2018-05-12 11:52:44 +0900

    fix import fonts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  WEB_FONT_URLS = [
+    'https://fonts.googleapis.com/css?family=Patrick+Hand'.freeze,
+    'https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css'.freeze
+  ]
+
+  def web_font_link_tag
+    return if Rails.env.test?
+    stylesheet_link_tag(*WEB_FONT_URLS, media: 'all', 'data-turbolinks-track': 'reload')
+  end
 end
