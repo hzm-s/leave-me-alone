e2a3a3d77d15adf3439bbbde5bc45db6cedb07c4
haazime <h4zime@gmail.com>
2018-05-12 17:59:22 +0900

    new todo form

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,14 +4,22 @@ export default class extends Controller {
   static targets = [ 'field', 'submit' ]
 
   connect() {
-    setInterval(() => {
-      this.detect()
-    }, 100)
+    this.disable()
+    this.startDetection()
+  }
+
+  startDetection() {
+    this.detectTimer = setInterval(() => { this.detect() }, 100)
+  }
+
+  stopDetection() {
+    if (this.detectTimer) {
+      clearInterval(this.detectTimer)
+    }
   }
 
   detect() {
     this.scanFieldLength()
-      console.log(this.canSubmit())
     if (this.canSubmit()) {
       this.enable()
     } else {
@@ -21,15 +29,19 @@ export default class extends Controller {
 
   scanFieldLength() {
     this.fieldLength = 0
-    this.fieldLength += this.fieldTarget.value.length;
+    this.fieldTargets.forEach((el) => {
+      this.fieldLength += el.value.length;
+    })
   }
 
   enable() {
     this.submitTarget.style.visibility = 'visible'
+    this.submitTarget.disabled = false
   }
 
   disable() {
     this.submitTarget.style.visibility = 'hidden'
+    this.submitTarget.disabled = true
   }
 
   canSubmit() {
