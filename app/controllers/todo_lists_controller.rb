07257b09cf62049c6e0c4354c4b1faa10cd24809
haazime <h4zime@gmail.com>
2018-05-19 16:01:07 +0900

    show todo list

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,7 +1,17 @@
 class TodoListsController < ApplicationController
+  List = Struct.new(:title, :items)
+  ListItem = Struct.new(:content)
 
   def show
-    @list = TodoList.last || TodoList.create
-    @add_command = AddTodoCommand.new
+    @list = List.new(
+      'Todo',
+      [
+        ListItem.new('Alpha'),
+        ListItem.new('Bravo'),
+        ListItem.new('Charlie'),
+        ListItem.new('Delta'),
+        ListItem.new('Echo'),
+      ]
+    )
   end
 end
