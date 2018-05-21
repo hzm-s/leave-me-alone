9b3c9938086ffe6f92539390fd310b0f2f35cd11
haazime <h4zime@gmail.com>
2018-05-21 22:24:39 +0900

    show as relative time

---
+++ app/javascript/controllers/relative_time_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from 'stimulus';
+import moment from 'moment';
+
+export default class extends Controller {
+  initialize() {
+    moment.locale('ja')
+  }
+
+  connect() {
+    this.suffix = this.element.innerText;
+    this.update()
+    this.updateTimer = setInterval(() => { this.update() }, 1000 * 60)
+  }
+
+  disconnect() {
+    if (this.updateTimer) {
+      clearInterval(this.updateTimer)
+    }
+  }
+
+  update() {
+    const absoluteTime = this.data.get('time')
+    const relativeTime = this.convertToRelativeTime(absoluteTime)
+    const suffix = this.data.get('suffix')
+    this.element.innerText = `${relativeTime}${suffix}`
+  }
+
+  convertToRelativeTime(absoluteTime) {
+    return moment(absoluteTime, 'YYYY/MM/DD hh:mm:ss').fromNow()
+  }
+}
