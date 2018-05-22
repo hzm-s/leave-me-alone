c89a7af88577b17ab40503051aee8f32af1e75d0
haazime <h4zime@gmail.com>
2018-05-22 21:56:24 +0900

    configure root to todo_lists#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resource :todo_list, only: [:show, :update]
 
-  resources :todos, only: [:create]
-  patch 'todo_list/:todo_list_id/:todo_list_index' => 'todos#update', as: :todo
-
   resources :pages, only: [:index, :show]
-  root to: 'pages#index'
+
+  root to: 'todo_lists#show'
 end
