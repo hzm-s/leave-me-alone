1d41e155fea530dba3235a3e1880e9cfbe3a452a
haazime <h4zime@gmail.com>
2018-05-26 10:11:50 +0900

    inline edit for todo

--- spec/system/inline_edit_spec.rb
+++ spec/system/inline_edit_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe 'Inline Edit', type: :system, js: true do
   before do
-    TodoList.create
+    TodoList.new(title: 'Todo') do |list|
+      list.add(content: 'Item123')
+      list.save!
+    end
+
     visit todo_list_path
   end
 
@@ -18,6 +22,11 @@ describe 'Inline Edit', type: :system, js: true do
 
   describe 'Todo' do
     it do
+      first('.test-list-item').hover
+      click_on '編集する'
+      fill_in 'form[todos][0][content]', with: 'Item789'
+      click_button
+      expect(page).to have_content('Item789')
     end
   end
 end
