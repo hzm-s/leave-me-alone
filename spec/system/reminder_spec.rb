92d1ca9ac3f200160638f4448d6e30e64cbb23a9
haazime <h4zime@gmail.com>
2018-05-26 16:39:00 +0900

    refactor

--- spec/system/reminder_spec.rb
+++ spec/system/reminder_spec.rb
@@ -6,13 +6,16 @@ describe 'Reminder', type: :system, js: true do
   context 'when show list' do
     it do
       visit todo_list_path
-      raw_remind_at = find('#test-remind-at')['data-reminder-at']
-      remind_at = Time.zone.parse(raw_remind_at)
       expect(about_min(30.minutes.from_now)).to include(remind_at)
     end
   end
 
   private
+
+    def remind_at
+      raw_remind_at = find('#test-remind-at')['data-reminder-at']
+      Time.zone.parse(raw_remind_at)
+    end
 
     def about_min(base_min)
       (base_min - 1.minutes)..(base_min + 1.minutes)
