8ebd22ce462d7da7b440c07bd36c3b01f5cd7c33
haazime <h4zime@gmail.com>
2018-05-27 18:54:49 +0900

    guards

--- spec/requests/sign_in_with_google_spec.rb
+++ spec/requests/sign_in_with_google_spec.rb
@@ -31,18 +31,4 @@ describe 'Sign in with google' do
       expect(response.body).to include('もう一度ログインしてください')
     end
   end
-
-  context 'when signed in' do
-    let(:google_identity) { mock_google_sign_in_identity }
-
-    before do
-      sign_up_with_google(google_identity)
-      post google_session_path, params: { google_id_token: google_identity.token }, xhr: true
-    end
-
-    it do
-      post google_session_path, params: { google_id_token: google_identity.token }, xhr: true
-      expect(response.body).to include('ログインしています')
-    end
-  end
 end
---
+++ spec/requests/todo_lists_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+describe 'TodoLists' do
+end
