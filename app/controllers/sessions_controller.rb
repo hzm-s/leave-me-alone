b8a88e9bbf280f01ab8837e7e5ab3cfc938c9d0e
haazime <h4zime@gmail.com>
2018-05-28 21:15:47 +0900

    sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   before_action :require_guest, only: [:new]
 
   def destroy
+    cookies.delete(:user_id)
     redirect_to new_session_url, notice: 'ログアウトしました'
   end
 end
--- spec/requests/sign_out_spec.rb
+++ spec/requests/sign_out_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 describe 'Sign out' do
   context 'when signed in' do
-    it do
-      google_identity = mock_google_sign_in_identity
-      user = sign_up_with_google_identity(google_identity)
+    let(:google_identity) { mock_google_sign_in_identity }
 
+    before do
+      sign_up_with_google_identity(google_identity)
       post google_session_path, params: { google_id_token: google_identity.token }, xhr: true
+    end
 
+    it do
       delete session_path
-      follow_turbolinks_visit!
+      follow_redirect!
 
       aggregate_failures do
         expect(response.body).to include('ログアウトしました')
 
-        #get todo_list_path
-        #expect(response.body).to include('ログインしてください')
+        get todo_list_path
+        follow_redirect!
+        expect(response.body).to include('ログインしてください')
       end
     end
   end
