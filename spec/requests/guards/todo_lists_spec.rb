a986328a43da09a9d992b0f20992601d02c1f347
haazime <h4zime@gmail.com>
2018-05-28 21:20:55 +0900

    add action description

--- spec/requests/guards/todo_lists_spec.rb
+++ spec/requests/guards/todo_lists_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe 'TodoLists' do
-  context 'when not signed in' do
-    it do
-      get todo_list_path
-      follow_redirect!
-
-      expect(response.body).to include('ログインしてください')
+  describe '#show' do
+    context 'when not signed in' do
+      it do
+        get todo_list_path
+        follow_redirect!
+        expect(response.body).to include('ログインしてください')
+      end
     end
   end
 end
