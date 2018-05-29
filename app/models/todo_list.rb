414d000a064684c0e6318447b4f521e33289b0ef
Hajime Sueyoshi <h4zime@gmail.com>
2018-05-29 21:36:48 +0900

    fix (#48)

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -6,12 +6,6 @@ class TodoList < ApplicationRecord
     self.updated_at ||= Time.current
   end
 
-  class << self
-    def find_by_user_id(user_id)
-      last || create!
-    end
-  end
-
   def update_with(new_todo_list)
     self.title = new_todo_list.title
     self.todos = new_todo_list.todos
