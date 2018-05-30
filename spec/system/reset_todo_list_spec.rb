2b0da41d64a236e76f0926081438eb416360fb61
Hajime Sueyoshi <h4zime@gmail.com>
2018-05-30 23:49:28 +0900

    remove done items (#57)
    * remove done items
    
    * test auto save for remove dones

---
+++ spec/system/reset_todo_list_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Reset todo list', system: true, js: true do
+  let!(:user) { sign_up }
+
+  before do
+    todo_list = user.todo_list
+    todo_list.tap do |list|
+      list.add(content: 'Alpha', done: true)
+      list.add(content: 'Bravo', done: true)
+      list.add(content: 'Charlie')
+      list.save!
+    end
+
+
+    sign_in(user)
+    visit todo_list_path
+  end
+
+  it do
+    remove_dones
+    aggregate_failures do
+      expect(page).to_not have_content('Alpha')
+      expect(page).to_not have_content('Bravo')
+      expect(page).to have_content('Charlie')
+    end
+  end
+end
