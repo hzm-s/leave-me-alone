c3853d96edbef8596ed98974ef687f92d931555e
haazime <h4zime@gmail.com>
2018-05-31 22:14:55 +0900

    extract to build options for form

--- app/helpers/todo_list_helper.rb
+++ app/helpers/todo_list_helper.rb
@@ -1,3 +1,16 @@
 module TodoListHelper
   mattr_accessor :auto_save_interval
+
+  def todo_list_form_options(is_instant_mode = false)
+    {
+      url: todo_list_url,
+      method: :patch,
+      scope: :form,
+      data: {
+        controller: 'auto-submit',
+        auto_submit_interval: TodoListHelper.auto_save_interval,
+        target: 'reminder.subject'
+      }
+    }
+  end
 end
