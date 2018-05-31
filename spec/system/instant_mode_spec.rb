89db0b72b7029c56bcb5ac42cfecb532756ba729
haazime <h4zime@gmail.com>
2018-05-31 22:28:13 +0900

    add todo

--- spec/system/instant_mode_spec.rb
+++ spec/system/instant_mode_spec.rb
@@ -11,4 +11,19 @@ describe 'Instant mode', type: :system, js: true do
       expect(page).to have_content('Yarukoto')
     end
   end
+
+  describe 'Add a todo' do
+    it do
+      add_todo('Alpha')
+      expect(page).to have_content('Alpha')
+    end
+  end
+
+  xdescribe 'Edit a todo' do
+    it do
+      add_todo('Fox')
+      edit_todo(0, 'Foxtrot')
+      expect(page).to have_content('Foxtrot')
+    end
+  end
 end
