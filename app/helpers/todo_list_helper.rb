21a6c260306e5fdd4eae915a27b2f4a4c4d9c450
haazime <h4zime@gmail.com>
2018-06-01 21:17:16 +0900

    refactor

--- app/helpers/todo_list_helper.rb
+++ app/helpers/todo_list_helper.rb
@@ -2,18 +2,32 @@ module TodoListHelper
   mattr_accessor :auto_save_interval
 
   def todo_list_form_options(instant_mode = false)
-    base = {
-      url: todo_list_url,
-      method: :patch,
-      scope: :form,
-      data: {
-        target: 'reminder.subject'
-      }
-    }
-    return base if instant_mode
-    base.deep_merge(data: {
-      controller: 'auto-submit',
-      auto_submit_interval: TodoListHelper.auto_save_interval
-    })
+    return disalbe_auto_save if instant_mode
+    enable_auto_save
   end
+
+  private
+
+    def enable_auto_save
+      {
+        url: todo_list_url,
+        method: :patch,
+        scope: :form,
+        data: {
+          target: 'reminder.subject',
+          controller: 'auto-submit',
+          auto_submit_interval: TodoListHelper.auto_save_interval
+        }
+      }
+    end
+
+    def disalbe_auto_save
+      {
+        url: '#',
+        scope: :form,
+        data: {
+          target: 'reminder.subject'
+        }
+      }
+    end
 end
