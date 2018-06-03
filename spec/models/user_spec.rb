cbda055a14f1ddb94c13c61e5c6d6daf1af0ffe0
haazime <h4zime@gmail.com>
2018-06-03 10:59:48 +0900

    refactor

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,32 @@
 require 'rails_helper'
 
 describe User do
-  it do
-    user = User.create!(name: 'User')
-    todo_list = TodoList.new
+  describe '#own_todo_list' do
+    let(:user) { User.create!(name: 'User') }
 
-    user.own_todo_list(todo_list)
-    user.save!
+    it do
+      todo_list = TodoList.new
 
-    aggregate_failures do
-      expect(user.todo_list_id).to_not be_nil
-      expect(user.todo_list_id).to eq(todo_list.id)
+      user.own_todo_list(todo_list)
+      user.save!
+
+      aggregate_failures do
+        expect(user.todo_list_id).to_not be_nil
+        expect(user.todo_list_id).to eq(todo_list.id)
+      end
+    end
+
+    it do
+      guest = register_guest
+
+      todo_list = TodoList.find_by_guest_id(guest.id)
+      user.own_todo_list(todo_list)
+      user.save!
+
+      expect { guest.destroy! }.to_not raise_error
     end
   end
 
   it do
-    guest = register_guest
-    user = User.create!(name: 'User')
-
-    todo_list = TodoList.find_by_guest_id(guest.id)
-    user.own_todo_list(todo_list)
-    user.save!
-
-    expect { guest.destroy! }.to_not raise_error
   end
 end
