2b92b05b1adec51deeb999540489fc8ec5e20246
Hajime Sueyoshi <h4zime@gmail.com>
2018-06-03 14:07:38 +0900

    Feature/confirm clear todo (#67)
    * confirm when all remove
    
    * improve copy
    
    * fix dropdown link color
    
    * fix

--- spec/support/ui_support.rb
+++ spec/support/ui_support.rb
@@ -59,12 +59,13 @@ module UISupport
 
     def remove_dones
       open_todo_list_menu
-      click_on '完了したTodoをすべて削除'
+      click_on '完了したTodoを削除'
     end
 
     def remove_all
       open_todo_list_menu
-      click_on '未完了も含めてTodoをすべて削除'
+      click_on 'Todoをすべて削除'
+      page.driver.browser.switch_to.alert.accept
     end
 
     def wait_for_todo_list_save(reload: true)
