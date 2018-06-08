293e38642b780293976a9eb25c296d0b005cd6d9
haazime <h4zime@gmail.com>
2018-06-08 22:45:59 +0900

    refactor

--- app/javascript/controllers/todo_list_item_controller.js
+++ app/javascript/controllers/todo_list_item_controller.js
@@ -2,33 +2,27 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   initialize() {
-    const index = parseInt(this.data.get('index'))
-    const checkId = `todo-list-item-check-${index}`
-
-    this.checkbox = this.element.querySelector('input[type="checkbox"]')
-    this.checkbox.id = checkId
-    this.checkbox.name = `form[todos][${index}][done]`
-
-    this.label = this.element.querySelector('label')
-
-    this.input = this.element.querySelector('input[type="text"]')
-    this.input.name = `form[todos][${index}][content]`
+    this.setCheckboxElement()
+    this.setLabelElement()
+    this.setInputElement()
   }
 
   connect() {
-    this.observeTimer = setInterval(() => {
-      let textDecoration = 'none'
-      if (this.checkbox.checked) {
-        textDecoration = 'line-through'
-      }
-      this.label.style.textDecoration = textDecoration
-    }, 200)
+    this.observeTimer = setInterval(() => { this.updateLabel() }, 200)
   }
 
   disconnect() {
     if (this.observeTimer) {
       clearInterval(this.observeTimer)
     }
+  }
+
+  updateLabel() {
+    let textDecoration = 'none'
+    if (this.isDone) {
+      textDecoration = 'line-through'
+    }
+    this.label.style.textDecoration = textDecoration
   }
 
   highlight(e) {
@@ -41,5 +35,30 @@ export default class extends Controller {
     e.preventDefault()
     const event = new Event('todoremoved', { bubbles: true })
     this.element.dispatchEvent(event)
+  }
+
+  setCheckboxElement() {
+    const checkId = `todo-list-item-check-${this.index}`
+
+    this.checkbox = this.element.querySelector('input[type="checkbox"]')
+    this.checkbox.id = checkId
+    this.checkbox.name = `form[todos][${this.index}][done]`
+  }
+
+  setLabelElement() {
+    this.label = this.element.querySelector('label')
+  }
+
+  setInputElement() {
+    this.input = this.element.querySelector('input[type="text"]')
+    this.input.name = `form[todos][${this.index}][content]`
+  }
+
+  get isDone() {
+    return this.checkbox.checked
+  }
+
+  get index() {
+    return parseInt(this.data.get('index'))
   }
 }
